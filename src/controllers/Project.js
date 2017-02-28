9841c0f407e95949176826a5152fdb7821f746b3
Sarah Federman <sarahbethfederman@gmail.com>
2017-02-27 22:26:50 -0800

    fixing prevnext

--- src/controllers/Project.js
+++ src/controllers/Project.js
@@ -23,80 +23,10 @@ var getAllProjects = function(req, res, mainContent) {
 };
 
 var getProject = function(req, res) {
-	//var composeRefs = async.compose(getNext, getPrev);
-
 	Project.findBySlug(req.params.slug, function(err, project) {
-    Project.findOne({_id: {$lt: project._id}}).sort({_id: -1}).exec(function(err, result) {
-      if (err) { console.log(err); }
-      if (result) {
-        console.log(result);
-        project.set('previous', result.slug);
-      }
-    });
-
-    Project.findOne({_id: {$gt: project._id}}).sort({_id: 1}).exec(function(err, result) {
-      if (err) { throw err; }
-      if (result) {
-        console.log(result);
-        project.set('next', result.slug);
-      }
-    });
-    
     res.render('project', project);
-		// projectCount(function() {
-      
-			// // once we have the count saved
-			// composeRefs(project, function(err, result) {
-				
-			// });
-		// });
 	});
 };
-
-
-function getNext(project, callback) {
-	var search;
-
-	// if its the last project
-	if (project.order >= totalProjects-1) {
-		// the next project is the first project
-		search = { order: 0 };
-	} else {
-		// else it's the next in order
-		search = { order: project.order + 1 };
-	}
-
-	// set the next property to the correct project slug
-	Project.findOne(search, 'slug', function(err, result) {
-    if (err) console.log(err);
-
-		project.set('next', result.slug);
-
-		// pass the transformed current project to getPrev
-		callback(null, project);
-	});
-}
-
-function getPrev(project, callback) {
-	var search;
-
-	// if it's the first project
-	if (project.order <= 0) {
-		// the previous project is the last project
-		search = { order: totalProjects-1 };
-	} else {
-		// else its the one before it
-		search = { order: project.order -1 };
-	}
-
-	// set the prev property to the correct project slug
-	Project.findOne(search, 'slug', function(err, result) {
-		project.set('prev', result.slug);
-
-		// pass the transformed project with prev & next set to the final project
-		callback(null, project);
-	});
-}
 
 module.exports.getAllProjects = getAllProjects;
 module.exports.getProject = getProject;
